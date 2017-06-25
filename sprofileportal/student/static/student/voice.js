9645c1519acd2a95d37de2cc7998005d3441e9bd
Arindam Das Modak <iit2016036@iiita.ac.in>
2017-06-25 14:39:14 +0530

    voice recognition js script

---
+++ sprofileportal/student/static/student/voice.js
@@ -0,0 +1,60 @@
+function startDictation() {
+
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+      var recognition = new webkitSpeechRecognition();
+
+      recognition.continuous = false;
+      recognition.interimResults = false;
+
+      recognition.lang = "en-IN";
+      recognition.start();
+      
+      recognition.onresult = function(e) {
+        var speech1 = (e.results[0][0].transcript).trim();
+        var speech2 = "";
+        for(i=0;i<speech1.length;i++)
+        {
+          if(speech1[i]!=" ")
+          {
+            speech2+=speech1[i].toUpperCase();
+          }
+        }
+        document.getElementById('q').value
+                                 = speech2;
+        recognition.stop();
+        document.getElementById('searchbtn').click();
+      };
+
+      recognition.onerror = function(e) {
+        recognition.stop();
+      }
+
+    }
+  }
+
+  function startDictation1() {
+
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+      var recognition = new webkitSpeechRecognition();
+
+      recognition.continuous = false;
+      recognition.interimResults = false;
+
+      recognition.lang = "en-IN";
+      recognition.start();
+
+      recognition.onresult = function(e) {
+        document.getElementById('msg1').value
+                                 = e.results[0][0].transcript;
+        recognition.stop();
+        document.getElementById('buttons1').click();
+      };
+
+      recognition.onerror = function(e) {
+        recognition.stop();
+      }
+
+    }
+  }
