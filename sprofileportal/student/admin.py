57b245aae48ed83e8aac51b03af35b029af1089c
Arindam Das Modak <iit2016036@iiita.ac.in>
2017-07-05 17:08:40 +0530

    GithubRank added

--- sprofileportal/student/admin.py
+++ sprofileportal/student/admin.py
@@ -4,9 +4,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Student, Site, StudentSite
+from .models import Student, Site, StudentSite, GithubRank
 
 admin.site.register(Student)
 admin.site.register(Site)
 admin.site.register(StudentSite)
+admin.site.register(GithubRank)
 
