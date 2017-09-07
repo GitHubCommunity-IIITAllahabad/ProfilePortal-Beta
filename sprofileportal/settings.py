ec3ec03d67001e7b6ec878cebb004376428b0d8f
RavicharanN <ravicharan.vsp@gmail.com>
2017-09-08 03:53:38 +0530

    Fix the db not displayed error

--- sprofileportal/settings.py
+++ sprofileportal/settings.py
@@ -17,7 +17,7 @@ EMAIL_PORT = 587
 
 import os
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,12 +81,9 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': dj_database_url.config()
+}
 try:
     from .local_settings import *
 except ImportError:
